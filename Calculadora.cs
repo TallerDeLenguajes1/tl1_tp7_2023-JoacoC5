02012faf7c2d94a480371c77c6131fd89053fd59
JoacoC5 <joacocarro05@gmail.com>
2023-06-01 19:01:31 -0300

    ejercicio 1

---
+++ Calculadora.cs
@@ -0,0 +1,38 @@
+namespace EspacioCalculadora;
+
+public class Calculadora
+{
+    private double valor;
+
+    public Calculadora()
+    {
+        valor = 0;
+    }
+
+    public double Resultado { get => valor; }
+
+    public void Suma(double sumando)
+    {
+        valor += sumando;
+    }
+
+    public void Resta(double restando)
+    {
+        valor -= restando;
+    }
+
+    public void Multiplicar(double multiplicador)
+    {
+        valor = valor * multiplicador;
+    }
+
+    public void Dividir(double divisor)
+    {
+        valor = valor / divisor;
+    }
+
+    public void Limpiar()
+    {
+        valor = 0;
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,53 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using EspacioCalculadora;
+
+Calculadora cal = new Calculadora();
+int seguir = 1;
+
+do
+{
+    Console.WriteLine("-----Calculadora Basica-----\n");
+    Console.WriteLine("1.Suma\n2.Resta\n3.Multiplicacion\n4.Division\n5.Limpiar");
+    int operacion;
+    int.TryParse(Console.ReadLine(), out operacion);
+
+    Console.WriteLine("\nIngrese un numero: ");
+    double x;
+    bool control = double.TryParse(Console.ReadLine(), out x);
+
+    if (control)
+    {
+        switch (operacion)
+        {
+            case 1:
+                cal.Suma(x);
+                break;
+            case 2:
+                cal.Resta(x);
+                break;
+            case 3:
+                cal.Multiplicar(x);
+                break;
+            case 4:
+                cal.Dividir(x);
+                break;
+            case 5:
+                cal.Limpiar();
+                break;
+            default:
+                Console.WriteLine("\nOperacioon no valida\n");
+                break;
+        }
+
+        Console.WriteLine("Resultado: " + cal.Resultado);
+    }
+    else
+    {
+        Console.WriteLine("\nNo se ingreso un numero");
+    }
+
+    Console.WriteLine("¿Desea realizar otra operacion? //1 = si //");
+    int.TryParse(Console.ReadLine(), out seguir);
+
+} while (seguir == 1);
